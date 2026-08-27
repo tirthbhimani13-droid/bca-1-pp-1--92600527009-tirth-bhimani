d2dac15dead7873727d01d00fcf6174e87c5f2e6
tirthbhimani13-droid <tirthbhimani13@gmail.com>
2026-08-27 15:59:13 +0530

    Add files via upload

---
+++ ASCII2.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+{
+	char x;
+	clrscr();
+	printf("\n enter any capital lettere : ");
+	scanf("%c",&x);
+
+	printf("\n small lettere %c",x-32);
+
+	getch();
+}
